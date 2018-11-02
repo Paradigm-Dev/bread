7764857c29034bbbeed5f665c40a63654282edfc
AidanLiddy <aidanliddy@outlook.com>
2018-11-01 21:15:03 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = angular.module('chatApp', ['firebase']);
+
+app.controller('ChatController', function($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child('messages');
+    $scope.messages = $firebaseArray(ref);
+        var ref = firebase.database().ref().child('usernames');
+    $scope.usernames = $firebaseArray(ref);
+
+    $scope.send = function() {
+        $scope.messages.$add({
+            message: $scope.messageText,
+            date: Date.now(),
+            username: $scope.usernameText
+        })
+    }
+
+})
