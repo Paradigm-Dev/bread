13055eb2322305550373d5ff6ed7271c1e5584b9
Aidan Liddy <aidanliddy@outlook.com>
2019-02-27 16:09:00 -0500

    DEPRECATION part 4

---
+++ functions/index.js
@@ -0,0 +1,67 @@
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
+  const fullName = user.displayName || 'Anonymous';
+  console.log(fullName + 'A new user signed in for the first time.');
+  var date = new Date(Date.now())
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  await admin.database().ref('messages').push({
+    name: 'Bot',
+    profilePicUrl: './assets/P.png', // Firebase logo
+    text: `${fullName} logged on for the first time!`,
+    date: date.toLocaleDateString(),
+    time: date.toLocaleTimeString()
+  });
+  console.log('Welcome message written to database.');
+});
+
+// Sends a notifications to all users when a new message is posted.
+exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCreate(
+    async (snapshot) => {
+      // Notification details.
+      const text = snapshot.val().text;
+      const payload = {
+        notification: {
+          title: `${snapshot.val().name} posted ${text ? 'a message' : 'an image'}`,
+          body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+          icon: snapshot.val().photoUrl || './assets/P.png',
+          click_action: `https://www.theparadigm.ga/flamechat.html`,
+        }
+      };
+
+      // Get the list of device tokens.
+      const allTokens = await admin.database().ref('fcmTokens').once('value');
+      if (allTokens.exists()) {
+        // Listing all device tokens to send a notification to.
+        const tokens = Object.keys(allTokens.val());
+
+        // Send notifications to all tokens.
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        await cleanupTokens(response, tokens);
+        console.log('Notifications have been sent and tokens cleaned up.');
+      }
+    });
+
+// Cleans up the tokens that are no longer valid.
+function cleanupTokens(response, tokens) {
+ // For each notification we check if there was an error.
+ const tokensToRemove = {};
+ response.results.forEach((result, index) => {
+   const error = result.error;
+   if (error) {
+     console.error('Failure sending notification to', tokens[index], error);
+     // Cleanup the tokens who are not registered anymore.
+     if (error.code === 'messaging/invalid-registration-token' ||
+         error.code === 'messaging/registration-token-not-registered') {
+       tokensToRemove[`/fcmTokens/${tokens[index]}`] = null;
+     }
+   }
+ });
+ return admin.database().ref().update(tokensToRemove);
+}
